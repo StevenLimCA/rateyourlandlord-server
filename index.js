d781fad7198dd0b4b1c586e425bba78dcca93382
Steven Lim <steven-lim-ca>
2022-06-28 14:02:35 -0700

    added public file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 8080;
 const cors = require("cors");
 const listing = require("./routes/listing");
 app.use(express.json());
+app.use(express.static("public"));
 app.use(cors());
 app.use("/listing", listing);
 app.listen(port, () => {
